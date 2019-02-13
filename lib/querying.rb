0e261a733b1844e706c1513b34b9a9a89eb47905
Robert McCary <robert.mccary@yahoo.com>
2019-02-13 17:26:04 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -10,7 +10,8 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(species) FROM characters
+  ORDER BY COUNT(species) LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
